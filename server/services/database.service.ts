23efbcc0d41e684b45a63804b6d2ed622a55fc2a
alewilliam789 <alewilliam789@gmail.com>
2023-05-09 09:48:54 -0700

    Remove the connection file and add db.service file
    This file will configure the connection and create the collections
    in one file instead of having separate conn file.

---
+++ server/services/database.service.ts
@@ -0,0 +1,28 @@
+// External Dependencies
+import * as mongoDB from "mongodb";
+import * as dotenv from "dotenv";
+
+
+
+// Global Variables
+export const collections : {notes?: mongoDB.Collection, users?: mongoDB.Collection} = {};
+
+export async function connectToDatabase(){
+    dotenv.config();
+    
+    const client: mongoDB.MongoClient = new mongoDB.MongoClient(process.env.DB_CONN_STRING);
+    
+    await client.connect;
+
+    const db: mongoDB.Db = client.db(process.env.DB_NAME);
+
+    const notesCollection: mongoDB.Collection = db.collection(process.env.NOTES_COLLECTION_NAME);
+
+    const usersCollection: mongoDB.Collection = db.collection(process.env.USERS_COLLECTION_NAME);
+
+    collections.notes = notesCollection;
+    collections.users = usersCollection;
+
+        console.log(`Successfully connected to database: ${db.databaseName} and collections: ${notesCollection.collectionName} & ${usersCollection.collectionName}`)
+
+}
