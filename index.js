0a28c19d3b5c05f60b70862c8575703d41669a18
alewilliam789 <alewilliam789@gmail.com>
2023-05-21 11:45:25 -0700

    publish: Added option for different port in index
    generated from commit 22d5968837b8f85476964f9e5fa91a29c6c26c15

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ import express from "express";
 import { connectToDatabase } from "./services/database.service.js";
 import { usersRouter } from "./routes/users.router.js";
 import { notesRouter } from "./routes/notes.router.js";
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 const app = express();
 connectToDatabase()
     .then(() => {
     app.use("/users", usersRouter);
     app.use("/notes", notesRouter);
     app.listen(port, () => {
-        console.log(`Server started at http://localhost:${port}`);
+        console.log(`Server started at ${port}`);
     });
 })
     .catch((error) => {
