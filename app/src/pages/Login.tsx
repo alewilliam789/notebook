dc18ba43a58041ce1e511f19f7d075fd37905cf7
alewilliam789 <alewilliam789@gmail.com>
2023-05-26 09:52:41 -0700

    Configure the fetch api calls to work properly
    Didn't specify content type (JSON), so I added the appropriate header.

--- app/src/pages/Login.tsx
+++ app/src/pages/Login.tsx
@@ -1,9 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import {useForm} from "react-hook-form";
-import { Link } from "react-router-dom";
+import { Link, useNavigate} from "react-router-dom";
 
 export default function Login(){
 
+    const navigate = useNavigate();
 
     const {register, handleSubmit, formState: {errors}} = useForm(
         {defaultValues: {
@@ -17,13 +18,30 @@ export default function Login(){
         password : string;
     }
 
-    const onSubmit= (data : FormValues) => {console.log(data)};
+
+    async function onSubmit(data: FormValues) {
+        console.log(JSON.stringify(data))
+            let response = await fetch('https://tayjournal-api.herokuapp.com/users/login',
+                        {
+                            method: "POST",
+                            headers: {'Content-Type':'application/json'},
+                            body: JSON.stringify(data),
+                        }
+                        )
+            console.log(response)
+            if(!response.ok){
+                navigate("/invalid-login");
+            }
+            else{
+                navigate("/notebook")
+            }
+        }
 
     return (
     <>
     <div className="w-80  mx-auto mt-20 border border-gray-800 rounded-lg">
-    <form id="user" className="p-10 flex flex-col gap-5" onSubmit={handleSubmit(onSubmit)}>
-            <label className="mb-10 self-center text-2xl"> Welcome</label>
+    <form id="user" className="p-10 grid gap-5" onSubmit={handleSubmit(onSubmit)}>
+            <label className="mb-10 text-center text-2xl"> Welcome</label>
             <input className="border-b-2 border-gray-300 font-mono focus:outline-none" placeholder="Username" {...register("userName", {required: "This field is required"})}/>
             <p className="text-red-600 italic font-thin text-sm">{errors.userName?.message?.toString()}</p>
             
