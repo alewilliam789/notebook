56dfbf1994dafffb52674f0c5db4977c6d6789a3
alewilliam789 <alewilliam789@gmail.com>
2023-06-30 14:31:33 -0700

    Style the scrollbar to fit the theme and style

--- app/src/components/SubmitButton.tsx
+++ app/src/components/SubmitButton.tsx
@@ -3,6 +3,6 @@
 export default function SubmitButton (){
 
     return (
-        <input className="mb-2 p-2 border rounded-xl text-white bg-gradient-to-r from-sky-500 to-indigo-500" type="submit" />
+        <input className="mb-2 p-2 border rounded-xl text-white bg-red-300" type="submit" />
     )
 }
