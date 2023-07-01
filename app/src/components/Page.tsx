664dd686dd3311667d4a8056995e685dd421d473
alewilliam789 <alewilliam789@gmail.com>
2023-07-01 13:56:39 -0700

    Create the Parallax page component
    This will replace the NoteList that was originally rendering
    collapsible components

---
+++ app/src/components/Page.tsx
@@ -0,0 +1,19 @@
+import { ParallaxLayer } from "@react-spring/parallax";
+import NotePanel from './NotePanel';
+
+
+
+interface PageProps {
+    offset: number;
+    handleClick?: () => void
+  }
+
+export default function Page({ offset, handleClick} : PageProps){
+
+    return(
+        <>
+        <ParallaxLayer className="pattern content" offset={offset} speed={0.3}>
+            <NotePanel />
+        </ParallaxLayer>
+        </>)
+};
